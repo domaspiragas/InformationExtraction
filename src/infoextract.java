bc328672f18c6e9c0683bd8ffd4519954b3727b7
Domas Piragas <domas.piragas@gmail.com>
2017-10-31 01:28:10 -0600

    Reading input file and separating individual stories. Structured main method. Created skeleton for generateTemplatesFromStories. Implemented findStoryId to get the story's id and store it in a template.

--- src/infoextract.java
+++ src/infoextract.java
@@ -1,13 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class infoextract {
     private static ArrayList<Template> templates = new ArrayList<>();
+    private static ArrayList<String> stories = new ArrayList<>();
 
     public static void main(String[] args){
 
+        /*//This would ideally be the total contents of main
+        * parseInputStories(args[0]);
+        *
+        * generateTemplatesFromStories();
+        *
+        * createTemplatesFile(args[0]);
+        * */
+
+        //Testing parse input stories
+        /*parseInputStories("resources/test.txt");
+        for(String s: stories){
+            System.out.println(s);
+        }*/
+
         //Testing output format
-        Template temp = new Template();
+       /* Template temp = new Template();
 
         temp
                 .setId("DEV-MUC3-0046")
@@ -25,7 +43,7 @@ public class infoextract {
 
         System.out.println(temp.toString());
 
-        createTemplatesFile("news.txt");
+        createTemplatesFile("news.txt"); */
     }
 
     /**Generates a file named < fileName >.templates which contains the templates
@@ -37,5 +55,54 @@ public class infoextract {
             }
         }catch (Exception e){
             System.out.println("Error: Failed to create: " + fileName + ".templates");}
+    }
+
+    /**Reads an input file containing stories separated by one of three different headers:
+     * DEV-MUC3-XXXX, TST1-MUC3-XXXX, or TST2-MUC4-XXXX (where the X’s are digits)
+     * Each story is stored separately from the others*/
+    private static void parseInputStories(String fileName){
+        String line;
+        StringBuilder story = new StringBuilder();
+        try {
+            FileReader fileReader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            while ((line = bufferedReader.readLine()) != null) {
+                if(line.startsWith("DEV-MUC") || line.startsWith("TST1-MUC3") || line.startsWith("TST2-MUC4")){
+                    // The first pass will be empty, this check is to avoid adding an empty string to our stories
+                    if(!story.toString().isEmpty()){
+                        stories.add(story.toString());
+                        story.setLength(0);
+                    }
+                }
+                // Not sure if it's necessary to preserve the new lines
+                story.append(line + "\n");
+            }
+            stories.add(story.toString());
+
+        } catch (IOException ex) {
+            System.out.println("Error: Could not read given input stories file: " + fileName + ".");
+        }
+    }
+
+    /**Parses and creates a template for each story and stores it*/
+    private static void generateTemplatesFromStories(){
+        Template template;
+        for(String story : stories) {
+            template = new Template()
+                    .setId(findStoryId(story));
+            //The the logic for parsing the story and building the template should be here
+            //Incident
+            //Weapon
+            //Perp Indiv
+            //Perp Org
+            //Target
+            //Victim
+            templates.add(template);
+        }
+    }
+    /**Used for retrieving the ID of a story. We take advantage of the fact that the first
+     * line will always be formatted in a way where the ID is followed by a '(' */
+    private static String findStoryId(String story){
+        return story.substring(0, story.indexOf("(")).trim();
     }
 }
